95cfc04e2c7c23e7b8874cc589d3629d71349c91
Eriche Gonzales <erichelouise@gmail.com>
2022-09-23 17:43:53 -0400

    set up django rest framework

--- server/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-   name = models.CharField(max_length=100)
-   username = models.CharField(max_length=100)
-   email = models.EmailField(max_length=100)
-
-   def _str_(self):
-       return self.name
