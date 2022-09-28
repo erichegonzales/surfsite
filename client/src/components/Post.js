6bc4cfb05bb6c7eeddf31b501083a89d9d1aa9d3
Eriche Gonzales <erichelouise@gmail.com>
2022-09-27 20:15:36 -0400

    added image and video feature to feed

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,12 +1,15 @@
 import { Card, CardGroup } from "react-bootstrap";
+import Video from "./Video";
 
 const Post = ({ post }) => {
+
   return (
     <CardGroup>
       <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>id: {post.id} </Card.Title>
           <Card.Img src={post.image}></Card.Img>
+            { post.video === null ? "" : <Video src={post.video} />}
           <Card.Text>comment: {post.content}</Card.Text>
         </Card.Body>
       </Card>
